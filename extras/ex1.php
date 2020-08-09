d527ec071751764f25889ef88d1c17fdf58f36a9
Marcus Marçal <marcus.marcal@gmail.com>
2020-08-09 20:51:18 -0300

    pasta alterada

---
+++ extras/ex1.php
@@ -0,0 +1,13 @@
+<?php
+$n1 = 4;
+$n2 = 10;
+
+$ad1 = $n1 + $n2;
+
+if($ad1 > 20){
+    $ad2 = $ad1 + 8;
+    echo $ad2;
+} else {
+    $ad2 = $ad1 - 5;
+    echo $ad2;
+}
---
+++ extras/ex2.php
@@ -0,0 +1,12 @@
+<?php
+$n = 130;
+
+$d1 = 10;
+$d2 = 5;
+$d3 = 2;
+
+echo "O número é $n" . PHP_EOL;
+if ($n % $d1 == 0) echo "$n é divisível por $d1" . PHP_EOL; else echo "$n não é divisível por $d1" . PHP_EOL;
+if ($n % $d2 == 0) echo "$n é divisível por $d2" . PHP_EOL; else echo "$n não é divisível por $d2" . PHP_EOL;
+if ($n % $d3 == 0) echo "$n é divisível por $d3" . PHP_EOL; else echo "$n não é divisível por $d3" . PHP_EOL;
+if ($n % $d1 != 0 and $n % $d2 != 0 and $n % $d3 != 0) echo "$n não é divisível nem por $d1, nem por $d2, nem por $d3... número maluco" . PHP_EOL;
